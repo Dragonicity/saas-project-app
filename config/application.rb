b6025ba544de6e8696d1349a3274f03c3704f06e
Mark-Nelson <mark.nelson@dragonicity.com>
2016-07-21 19:05:47 +0800

    Carrierwave is total shit

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 require 'carrierwave'
 
+config.autoload_paths += "#{Rails.root}/app/uploaders"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
